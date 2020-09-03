5fd4c7a0467efb97906adcfb716aeeefaf727e12
boomini <bomin2641@gmail.com>
2020-09-03 16:40:02 +0900

    vusjs환경설정

---
+++ vuejs/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>check</h2>
+    <h3 v-for="(user, index) in users" :key="user.id">
+      <router-link :to="{ name: 'DetailUser', params: { id: index } }"
+        >{{ user.name }} : {{ user.pnum }}</router-link
+      >
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "first",
+      users: []
+    };
+  },
+  created() {
+    this.$http.get("/api/users").then(resp => {
+      this.users = resp.data.response;
+    });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
